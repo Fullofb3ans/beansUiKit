8249b8df05b2a6343c29a4801a9fa6c583f69fda
Fullofb3ans <igorgazinskii6@gmail.com>
2026-05-06 17:40:44 +0400

    bug/вернул watch

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -43,7 +43,7 @@
 </template>
 
 <script lang='ts'>
-import { defineComponent, ref, type Component, type PropType } from 'vue';
+import { defineComponent, ref, type Component, type PropType, watch } from 'vue';
 
 interface BaseSelectProps {
     propsClass?: string;
@@ -101,7 +101,12 @@ export default defineComponent({
         }
     },
     setup(props: BaseSelectProps, { emit }) {
-        const selectValue = ref(props?.propsValue || "");
+        const selectValue = ref("");
+
+        watch((props), () => {
+            if (props.propsValue)
+                selectValue.value = String(props.propsValue);
+        })
 
         const valueChanged = () => {
             emit('valueChanged', selectValue.value)
