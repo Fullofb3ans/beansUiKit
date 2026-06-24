e5c3255dd652f6fcc0078814295ad27c9b6fe503
Fullofb3ans <igorgazinskii6@gmail.com>
2026-06-24 12:39:13 +0400

    bug/add textarea export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import BaseButton from "./BaseButton.vue";
 import BaseSelect from "./BaseSelect.vue";
 import BaseInput from "./BaseInput.vue";
+import BaseTextarea from "./BaseTextarea.vue";
 
 export {
     BaseButton,
     BaseSelect,
-    BaseInput
+    BaseInput,
+    BaseTextarea
 }
